761de2af1f2ac472486c56b5d0a3f53c6740dcc3
Hasnae Amr <150762808+HasnaeAmr@users.noreply.github.com>
2024-04-29 19:43:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ let v = document.querySelector('.card');
 window.onscroll = function() {
     let scrollValue = window.scrollY; // corrected from scrollY to window.scrollY
 }
+function sendMail() {
+    var params = {
+        fullName: document.getElementById("fullName").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value, // Added .value
+        msg: document.getElementById("msg").value // Added .value
+    };
+
+    emailjs.send("service_prqrs1m", "template_erv145b", params)
+        .then(function (res) {
+            alert("Success! " + res.status);
+        })
+        .catch(function (error) {
+            alert("Error: " + error);
+        });
+}
