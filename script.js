d58106e77bfad43999c932d93682bc5668e72abc
Hasnae Amr <150762808+HasnaeAmr@users.noreply.github.com>
2024-09-10 02:03:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let v = document.querySelector('.card');
+
 window.onscroll = function() {
-    let scrollValue = window.scrollY; // corrected from scrollY to window.scrollY
-}
+    var scrollButton = document.querySelector('.upScroll');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+};
+
+(function(){
+    emailjs.init('KYdl5kqfD4ccMMruc');
+})();
 function sendMail() {
     var params = {
         fullName: document.getElementById("fullName").value,
         email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value, // Added .value
-        msg: document.getElementById("msg").value // Added .value
+        subject: document.getElementById("subject").value, 
+        msg: document.getElementById("msg").value 
     };
 
     emailjs.send("service_prqrs1m", "template_erv145b", params)
@@ -15,6 +25,9 @@ function sendMail() {
             alert("Success! " + res.status);
         })
         .catch(function (error) {
-            alert("Error: " + error);
+            alert("Failure!: " + error);
         });
 }
+
+
+
